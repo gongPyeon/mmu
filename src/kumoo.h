ab10c8c39d1a8c88ff88ef6ada2afe66dd5fb730
gongPyeon <pyeonkang@naver.com>
2024-05-03 20:41:40 +0900

    [update]: ku_pagfault_handler 구현

--- src/kumoo.h
+++ src/kumoo.h
@@ -67,10 +67,10 @@ void ku_freelist_init(void){
 }
 
 
-int ku_pgfault_handler(unsigned short virtualADDR){ //16비트
+int ku_pgfault_handler(unsigned short virtualADDR){ //16비트 (2byte)
     //접근하려는 vpn이 page table entry의 개수보다 클때
-    unsigned short offset = (virtualADDR & OFFSET_MASK);
-    unsigned short VPN = (virtualADDR & VPN_MASK) >> VPN_SHIFT;
+    short offset = (virtualADDR & OFFSET_MASK);
+    short VPN = (virtualADDR & VPN_MASK) >> VPN_SHIFT;
     if(VPN >= PFNUM) return 0;
 
     unsigned short PDindex = (VPN & PD_MASK) >> SHIFT;
@@ -80,14 +80,20 @@ int ku_pgfault_handler(unsigned short virtualADDR){ //16비트
     int PFN = (*PDE & PFN_MASK) >> PFN_SHIFT;
     ptbr = (unsigned short*)(pmem+(PFN << 6));
     unsigned short PTindex = (VPN & PT_MASK);
-    unsigned char* PTE = ptbr + PTindex;
+    short* PTE = ptbr + PTindex;
 
     //접근하려는 pfn이 disk에 있을 때
     if((*PTE & 0x0001) == 0 && ((*PTE) << 1) != 0){ //present bit가 0인데, pfn이나 dirty가 0이 아니라면 swap out 되어있는 것
+        
+        int pfn = (*PTE & 0xFFFC) >> 2;
+        memset(&swaps[pfn * PAGE_SIZE], 0, PAGE_SIZE); //메모리에서 없애기
+        //present bit 1로 바꾸기 (비어있는 메모리에 넣을때 설정할 것이다)
+        swaps_free[pfn] = 0; //freelist 0으로 업데이트
+
+
          //pfn이 swap offset이므로 swap offset으로 접근한다
          //비어있는 메모리가 있으면 present bit을 1로 설정한다 / pfn을 재설정한다
          //비어있는 메모리가 없으면 (아래와 같음)
-         return 1;
     }
 
     //비어있는 메모리가 있을때
@@ -95,7 +101,6 @@ int ku_pgfault_handler(unsigned short virtualADDR){ //16비트
         if(pmem_free[i] == 0){
             *PTE = i << 4; // pfn 저장
             *PTE = *PTE | 0x0001; // present bit 1 설정
-            pmem_free[i] = 1;
 
             if(swapInList == NULL){
                 swapInList = (swapInPageNode *)malloc(sizeof(swapInPageNode));
@@ -112,27 +117,60 @@ int ku_pgfault_handler(unsigned short virtualADDR){ //16비트
                 temp->next->pteAddress = PTE;
                 temp->next->next = NULL;
             }
+            memcpy(&pmem[i * PAGE_SIZE], swapInList[i], PAGE_SIZE); //메모리에 해당 current pcb 적재
+            pmem_free[i] = 1;
             //swap in list에 추가한다
-            return 1;
+            return 0;
         }
+
+
     }
 
     //physical memory가 다 찼을 때
     
     for(int i = 0; i<SFNUM; i += PAGE_SIZE){
-        if((*PTE | 0x0002) == 0){ // dirty bit이 0일때
+        
+            if(swaps_free[i] == 0){ //swap space가 남았을 때
+
+                swapInPageNode *swapOutPage = swapInList;
+                swapInPageNode *eviction = swapInList; 
                 
-        }else{
-            if(swaps[i] == 0){
-            
-        }
-        //dirty bit이 0이면 그냥 버린다
+                if((*(eviction->pteAddress) | 0x0002) == 0){ // dirty bit이 0일때 쫓아내고 추가하기
+                    eviction->next = NULL; //첫번째거 쫓아내기 but page directory를 쫓아내면 안된다(how?)
+                }
+                else{ //dirty bit이 1일땐 swap space에 저장하고 추가하기
+                    *PTE = eviction->pteAddress; // pfn 저장
+                    *PTE = *PTE | 0x0000; // present bit 1 설정
+                    memcpy(&swaps[i * PAGE_SIZE], current, PAGE_SIZE); //메모리에 해당 current pcb 적재
+                    swaps_free[i] = 1;
+                    eviction->next = NULL;
+                } 
+
+
+                int pfn = *(eviction->pteAddress)&0xFFF0; //pfn을 얻어온다
+                *PTE = *(eviction->pteAddress) | 0x0001; // present bit 1 설정
+
+                memcpy(&pmem[pfn * PAGE_SIZE], eviction, PAGE_SIZE); //메모리에 해당 current pcb 적재
+                pmem_free[pfn] = 1;
+
+                //swap in list에 추가한다
+                swapInPageNode* temp = eviction;
+                while (temp->next != NULL) {
+                    temp = temp->next;
+                }
+                temp->next = (swapInPageNode*)malloc(sizeof(swapInPageNode));
+                temp->next->pid = current->pid;
+                temp->next->pteAddress = PTE;
+                temp->next->next = NULL;
+
+                swapInList = eviction;
+                return 0;
+        //dirty bit이 0이면 물리메모리에서 그냥 버린다
         //swap in list안에 있는 것 중 첫번째를 swap out(swaps)로 옮긴다 (FIFO)
         //옮겨진 swaps의 주소를 저장한다 (pfn)
         //pde, pte를 업데이트한다 (swap offset으로 변경 / present bit 1)
         }
     }
-
 
 }
 
@@ -187,7 +225,7 @@ int ku_proc_exit(unsigned short pid){ //종료시킬 pid가 넘어온다
             }
             free(current); //해당 pcb 해제
 
-            memset(&swaps[pidCount * PAGE_SIZE], 0, PAGE_SIZE);
+            memset(&pmem[pidCount * PAGE_SIZE], 0, PAGE_SIZE);
             swaps_free[pidCount] = 0;
 
             // pidCount 감소
@@ -263,7 +301,7 @@ void ku_proc_init(int argv1, char* argv2){
                 newProcessNode->pcblock.pgdir[i] = 0;
             }
             // swap에 pgdir만큼의 크기를 할당해야한다
-            memcpy(&swaps[pidCount * PAGE_SIZE], newProcessNode->pcblock.pgdir, PAGE_SIZE);
+            memcpy(&pmem[pidCount * PAGE_SIZE], newProcessNode->pcblock.pgdir, PAGE_SIZE);
             swaps_free[pidCount] = 1; //pidcount를 곱하는게 맞아?
         }
     }
